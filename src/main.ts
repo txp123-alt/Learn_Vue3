dad37b4705d26d0125a59cb5ff64ccb6f87ced3d
CompanyUser <1101642762@qq.com>
2025-05-22 18:18:40 +0800

    引入Element plus

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 //引入 Element Plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn' //国际化的配置
 
 const app = createApp(App);
 
