fcee41807bf1b9d91bbc0a1a6f655f246066bbb4
CompanyUser <1101642762@qq.com>
2025-05-26 13:24:57 +0800

    安装 vue type 编译器， 解决idea不识别 生命周期方法的问题

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <!--    <a href="https://vite.dev" target="_blank">-->
-    <!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-    <!--    </a>-->
-    <!--    <a href="https://vuejs.org/" target="_blank">-->
-    <!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-    <!--    </a>-->
     <h1>我是APP</h1>
     <SvgIcon name="time" width="100px" height="100px"></SvgIcon>
     <h1>测试Axios</h1>
+
   </div>
 </template>
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+onMounted(()=>{
+  console.log("组件挂载完成")
+})
 
 </script>
 <style scoped lang="scss">
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,5 +1,6 @@
 //用户相关接口
 import request from '@/utils/request.ts'
+import type {loginForm,loginResponseData, userResponseData} from '@/api/user/type.ts'
 
 //统一管理接口
 enum API {
@@ -8,7 +9,7 @@ enum API {
 }
 
 //对外暴露接口
-export const reqLogin = (data:any) => request.post(API.LOGIN_UEL,data);
+export const reqLogin = (data:loginForm) => request.post<any,loginResponseData>(API.LOGIN_UEL,data);
 
 //获取用户信息
-export const reqUserInfo = () => request.get(API.USERINFO_URL);
+export const reqUserInfo = () => request.get<any,userResponseData>(API.USERINFO_URL);
